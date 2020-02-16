3ecb6bb9dfef7ddbf1e98e11cdccd2ea83b62412
anand <anandcheerla999@gmail.com>
2020-02-16 20:50:06 +0530

    version so far

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function rock(x,y,img,id,lane){
 // ancestor,so that rock will always come from top
 
 rock.prototype.setUp=function(left_gap){
-	this.ele=$("<img src="+this.img+" id="+this.id+" class='rock' height=60 width=128>");
+	this.ele=$("<img src="+this.img+" id="+this.id+" class='rock' height=60 width=123>");
 	$(this.lane).append(this.ele);
 	this.ele.css({position:"absolute",left:left_gap+"px"});
 	
@@ -50,11 +50,13 @@ function car(x,y,img,id,lane){
 
 
 car.prototype.setUp=function(){
-	this.ele=$("<img src="+this.img+" id="+this.id+" class='car' height=100 width=128>");
+	this.ele=$("<img src="+this.img+" id="+this.id+" class='car' height=100 width=123>");
 	$(this.lane).append(this.ele);
 	//position set as relative so that car will move
 	this.ele.css({position:"relative"});
 	this.ele.css({top:"400px"});
+
+
 	
 };
 
@@ -78,8 +80,9 @@ var maxspeed=600;
 //rock creation on lane one and animate the rock
 let lane1rock=function(){
 		var rock1=new rock(0,0,"./images/rock_1.png","rock1","#lane1");
-		rock1.setUp(0);
+		rock1.setUp(1);
 		rock1.move(speed);
+		// console.log($("#rock1").position().left);
 		speed_counter-=10;
 		if(speed_counter<maxspeed)
 			speed_counter=speed;
@@ -88,8 +91,9 @@ let lane1rock=function(){
 //rock creation on lane two and animate the rock
 let lane2rock=function(){
 		var rock2=new rock(0,0,"./images/rock_2.svg","rock2","#lane2");
-		rock2.setUp(135);
+		rock2.setUp(126);
 		rock2.move(speed);
+		// console.log($("#rock2").position().left);
 		speed_counter-=10;
 		if(speed_counter<maxspeed)
 			speed_counter=speed;
@@ -98,7 +102,7 @@ let lane2rock=function(){
 //rock creation on lane three and animate the rock
 let lane3rock=function(){
 		var rock3=new rock(0,0,"./images/rock_3.png","rock3","#lane3");
-		rock3.setUp(0);
+		rock3.setUp(1);
 		rock3.move(speed);
 		speed_counter-=10;
 		if(speed_counter<maxspeed)
@@ -109,7 +113,7 @@ let lane3rock=function(){
 //rock creation on lane four and animate the rock
 let lane4rock=function(){
 		var rock4=new rock(0,0,"./images/rock_4.png","rock4","#lane4");
-		rock4.setUp(135);
+		rock4.setUp(126);
 		rock4.move(speed);
 		speed_counter-=10;
 		if(speed_counter<maxspeed)
@@ -122,7 +126,7 @@ let lane4rock=function(){
 //--------------------------------------------------------------------------------------------------------
 
 //interval logic so that two rocks come beside each other,
-var gap=800;
+var gap=1200;
 var movement=1;
 
 var interval=function(){
@@ -164,6 +168,8 @@ function interval4(){
 //---------------------------------------------------------------------------------------------------------
 
 function hits(){
+
+	// $("#car1")
 	$(".car").each(function(){
 		var car=$(this);
 			// console.log(car);
@@ -181,14 +187,16 @@ function hits(){
 				car_left=car.position().left;
 				car_top=car.position().top;
 
-				//console.log(rock.context.id+" "+rock_left+" "+car_left);
+				// console.log(rock.context.id+" "+rock_left+" "+car_left);
 				
 				
-				if(rock_top+60>car_top && (rock_left<car_left && car_left<rock_left+120)){
+				if((rock_top+60-5>=car_top && rock_top+5<=car_top) && (rock_left<=car_left && car_left<=rock_left+123)){
 					
-					console.log(car.context.id+" "+car_left+" "+rock_left);
+					console.log(rock.context.id+" "+car.context.id+" "+rock_left+" "+rock_top+" "+car_left+" "+car_top);
+					console.log("akc");
 					car.attr("src","./images/car_smashed_1.png");
-					movement=0;
+					rock.attr("src","./images/broken_rock_by_pantapuff_d4t54st-fullview.jpg");
+					//movement=0;
 					rock.stop();
 					
 				}
@@ -217,22 +225,26 @@ $(document).on("keydown",function(key){
 	if(key.which === 37){ 
 			key.preventDefault();
 			if(flag==0){
-				$('#car1').animate({left:"+=135px"},200,"swing");
+				$('#car1').animate({left:"+=125px"},200,"swing");
+				
 				flag=1;
 			}
 			else{
-				$('#car1').animate({left:"-=135px"},200,"swing");
+				$('#car1').animate({left:"-=125px"},200,"swing");
+				
 				flag=0;
 			}		
 	}
 	else if(key.which === 39){ 
 			key.preventDefault();
 			if(flag2==0){
-				$('#car2').animate({left:"+=135px"},200,"swing");
+				$('#car2').animate({left:"+=125px"},200,"swing");
+				
 				flag2=1;
 			}
 			else{
-				$('#car2').animate({left:"-=135px"},200,"swing");
+				$('#car2').animate({left:"-=125px"},200,"swing");
+				
 				flag2=0;
 			}		
 	}
